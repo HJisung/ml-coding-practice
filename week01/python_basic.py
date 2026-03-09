7774e57cf7f1f88425f09b75ecae87001c302318
HJisung <wltjd9972@gmail.com>
2026-03-09 22:41:43 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -404,4 +404,8 @@ print(sorted('Python'))
 # 패키지, 모듈 사용
 # Request('http://www.sunmoon.ac.kr')   # 오류 발생
 
-import urllib.requ
+import urllib.request
+urllib.request.Request('http://www.sunmoon.ac.kr')
+
+import pandas
+p
