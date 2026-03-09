f6baed76fd268cb9078b4b8b076f40e0aa23217b
HJisung <wltjd9972@gmail.com>
2026-03-09 22:35:41 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -349,4 +349,10 @@ print(dir({'1':'a'}))
 print(dir(1))
 
 print(int('3'))                # x를 정수 형태로 반환
-print(str(3))                  # x를 문자열 형태로 
+print(str(3))                  # x를 문자열 형태로 반환
+
+# x를 리스트로 반환
+print(list("Python"))
+print((1, 2, 3))
+
+# x를 튜플로 
