f9bed7e6f8a12c5bc046f6af5c4eb66c9c14ea72
HJisung <wltjd9972@gmail.com>
2026-03-18 15:43:59 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -145,4 +145,8 @@ df['average'] = df[['math', 'science', 'english']].mean(axis=1)
 print(df)
 
 # 이름과 평균값만을 포함하는 새로운 데이터프레임 생성
-average_df = df[['name', 'aver
+average_df = df[['name', 'average']]
+print(average_df)
+
+# 2.2 넘파이
+import numpy
