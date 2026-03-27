2093e53de7571122be321e66db22eddfd73a0752
HJisung <wltjd9972@gmail.com>
2026-03-28 00:27:56 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -310,4 +310,6 @@ plt.tick_params(axis='y', labelcolor='deeppink')
 plt.legend(loc='upper right')
 
 # 제목 설정
-plt.suptitle('Survival Analysis by Number of Parents/Children (Pa
+plt.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
+plt.tight_layout()
+plt.savefig
