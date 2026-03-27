9b8df4d676fab7499ae33f834471594e41550900
HJisung <wltjd9972@gmail.com>
2026-03-28 00:09:45 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -239,4 +239,7 @@ plt.close()
 fare_means = titanic.groupby('Parch')['Fare'].mean()  # 평균
 print(fare_means, '\n')
 
-fare_std = titanic.groupby('Parch')['Fare']
+fare_std = titanic.groupby('Parch')['Fare'].std()     # 표준 편차
+print(fare_std)
+
+"""* 에러바는 데이"""
