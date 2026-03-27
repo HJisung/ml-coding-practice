70f734fef4671e20839e85781d11ab346e652703
HJisung <wltjd9972@gmail.com>
2026-03-27 23:53:05 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -180,4 +180,6 @@ plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
 
 plt.title('Survival by Age Group on Titanic')
 plt.xlabel('Age')
-plt."""
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+plt.legend"""
