e874ef41cef4e198680e5d104cb78ccf84df6a0e
HJisung <wltjd9972@gmail.com>
2026-03-27 23:19:45 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -60,4 +60,7 @@ plt.close()
 
 # 성별에 따른 생존자의 수 계산
 survived_counts = titanic[titanic['Survived'] == 1]['Sex'].value_counts()
-print(survived_
+print(survived_counts)
+
+# 수평 막대 그래프 그리기
+bars = plt.barh(survive
