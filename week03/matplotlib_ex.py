cc21f8f554afe458aa84f6efd7caf9af7f5cc784
HJisung <wltjd9972@gmail.com>
2026-03-27 23:35:25 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -112,4 +112,9 @@ plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange', 
 
 plt.title('Survival Distribution on the Titanic')
 plt.savefig('Figure05.png')
-plt.close()"""
+plt.close()
+
+"""### **히스토그램 : 승객의 나이 분포 표시하기**"""
+
+# 처리 전
+"""
