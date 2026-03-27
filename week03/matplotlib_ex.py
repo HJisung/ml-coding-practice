227167c714fe64d3fc96a5a047b159ea8fbc7382
HJisung <wltjd9972@gmail.com>
2026-03-27 23:30:22 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -95,4 +95,5 @@ plt.title('Age and Fare Relationship with Survival on the Titanic')
 plt.xlabel('Age')
 plt.ylabel('Fare')
 plt.legend(handles=scatter.legend_elements()[0], title='Survived',
-           labels=['Not Surviv"""
+           labels=['Not Survived', 'Survived'], loc='upper right')
+plt.savefig('Fi"""
