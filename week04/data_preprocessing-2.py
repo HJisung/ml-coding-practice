8194f3ad2524d129bba576a4f49079dc8c57951c
HJisung <wltjd9972@gmail.com>
2026-04-03 17:47:19 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -70,4 +70,6 @@ housing_labels = housing_labels.iloc[outlier_pred == 1]
 housing_cat = housing[["ocean_proximity"]]
 housing_cat.head(8)
 
-from sklearn.preprocessi
+from sklearn.preprocessing import OrdinalEncoder
+
+ordinal_encoder = Ordina
