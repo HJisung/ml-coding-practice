44b0f66110a280296ec20ae2d023ee9c4adcae44
HJisung <wltjd9972@gmail.com>
2026-04-03 17:23:14 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -71,4 +71,6 @@ sns.histplot(tips['tip'], bins=30, kde=True, color='skyblue')
 plt.title('Histogram with KDE for Tips')
 plt.savefig('./week04/Seaborn_Figure04.jpg')
 
-# **조인트 
+# **조인트 그래프**
+# jointplot( ) 그리기
+sns.jointplot(x='size', y
