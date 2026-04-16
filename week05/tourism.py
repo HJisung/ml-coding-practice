97ca6e626db250c48150a8f67e07ad0a41c673bf
HJisung <wltjd9972@gmail.com>
2026-04-16 13:55:30 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -58,4 +58,4 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 """### [CODE 2]"""
 
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
-  service_url = "http://openapi.tour.go.kr/"""
+  service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTo"""
