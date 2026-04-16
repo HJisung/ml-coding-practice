471202d6842a14f16fde9db65b99348aefda9b7c
HJisung <wltjd9972@gmail.com>
2026-04-16 13:32:47 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -4,4 +4,6 @@ import datetime
 import json
 import pandas as pd
 
-ServiceKey = "13fed2f70a682cfaf906db5413bed88fac9
+ServiceKey = "13fed2f70a682cfaf906db5413bed88fac909abeb66712d653574e9fc0976b6d"
+
+"""### [CODE 0]""""""
