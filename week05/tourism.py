2e78888cad6169999abee0c08ae147e775f66d27
HJisung <wltjd9972@gmail.com>
2026-04-16 14:03:35 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -84,4 +84,5 @@ def getRequestUrl(url):  #[CODE 1]
       return response.read().decode('utf-8')
   except Exception as e:
     print(e)
-    print("[%s] Error for URL :
+    print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+    return 
