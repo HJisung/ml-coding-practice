836110fb02c8f5cedddadbbb2a85e0290e25c280
HJisung <wltjd9972@gmail.com>
2026-04-16 13:33:18 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -6,4 +6,9 @@ import pandas as pd
 
 ServiceKey = "13fed2f70a682cfaf906db5413bed88fac909abeb66712d653574e9fc0976b6d"
 
-"""### [CODE 0]""""""
+"""### [CODE 0]"""
+
+def main():
+  jsonResult = []
+  result = []
+"""
