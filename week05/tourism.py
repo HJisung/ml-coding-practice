ecd0c5d23314df3cdb2d4a60fc76d913052f9e95
HJisung <wltjd9972@gmail.com>
2026-04-16 13:54:29 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -55,4 +55,6 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 
   return (jsonResult, result, natName, ed)
 
-"""### [COD"""
+"""### [CODE 2]"""
+
+def getTourismStatsItem(yyyymm, nat_cd, """
