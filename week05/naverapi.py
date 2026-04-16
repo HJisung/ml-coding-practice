4e8235c6c1c4847ed084cd5161490a806dbdf4f5
HJisung <wltjd9972@gmail.com>
2026-04-16 13:09:09 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -44,3 +44,6 @@ def getNaverSearch(node, srcText, page_start, display):
 
     url = base + node + parameters
     responseDecode = getRequestUrl(url)               #[CODE 1]
+
+    if (responseDecode == None):
+        return
