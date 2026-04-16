602fe8a70b7294f4fb5bae154ced3e4a6d2809e9
HJisung <wltjd9972@gmail.com>
2026-04-16 13:21:16 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -83,4 +83,6 @@ def getPostData(post, jsonResult, cnt):  #[CODE 3]
     jsonResult.append({
         'cnt': cnt,
         'title': title,
-        'description
+        'description': description,
+        'org_link': org_link,
+   
