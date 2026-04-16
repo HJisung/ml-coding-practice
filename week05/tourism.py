ca52e92559aa41e21fff35ee4c6af96118e2cae3
HJisung <wltjd9972@gmail.com>
2026-04-16 13:41:52 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -32,4 +32,5 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
   result = []
 
   for year in range(nStartYear, nEndYear+1):
-    for mo
+    for month in range(1, 13):
+      yyyymm = "{0}{1:0>2}".f
