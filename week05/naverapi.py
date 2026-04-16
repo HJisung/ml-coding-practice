6f286ce90e6746bc8cf3bab33b161d9894cc4d7b
HJisung <wltjd9972@gmail.com>
2026-04-16 13:06:07 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -37,4 +37,5 @@ def main():
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
 
-def getNaverSearch(node, srcText, page_
+def getNaverSearch(node, srcText, page_start, display):
+    base = "https://openapi.naver.
