9470e3f76269d4f5d7de4b64e6b803a8ab401211
HJisung <wltjd9972@gmail.com>
2026-04-16 14:02:04 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -80,4 +80,5 @@ def getRequestUrl(url):  #[CODE 1]
   try:
     response = urllib.request.urlopen(req)
     if response.getcode() == 200:
-      print("[%s] Url Request Su
+      print("[%s] Url Request Success" % datetime.datetime.now())
+      return re
