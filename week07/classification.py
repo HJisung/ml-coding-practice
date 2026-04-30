127bdc9341682571fcfe97e749984e32b2673468
HJisung <wltjd9972@gmail.com>
2026-04-30 15:15:12 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -51,4 +51,6 @@ from sklearn.model_selection import cross_val_score
 
 cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
 
-from 
+from sklearn.dummy import DummyClassifier
+
+dummy_clf = D
