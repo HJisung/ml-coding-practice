79c328114b7f3b18bc22cac18dabc05b6218ba10
HJisung <wltjd9972@gmail.com>
2026-05-01 18:29:47 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -175,4 +175,4 @@ netflix_age_country_unstack = netflix_age_country_unstack.fillna(0)
 netflix_age_country_unstack
 
 # 나이 그룹에 따른 국가별 넷플릭스 콘텐츠 비율 구하기
-netfli
+netflix_age_country_unstack = netflix_age_country_unstack.d
