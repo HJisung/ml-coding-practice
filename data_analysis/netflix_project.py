4bc1bd66fafbd10b3e59dc7e495ad0679d8244bc
HJisung <wltjd9972@gmail.com>
2026-05-01 18:17:41 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -140,4 +140,6 @@ plt.ylabel('Genre', fontsize=14)
 plt.grid(axis='x')
 plt.show()
 
-netflix[netflix['title'].str.contains('Sankofa', 
+netflix[netflix['title'].str.contains('Sankofa', na=False, case=False)]
+
+# 출력할 최대 행 수를 None으로 설정해서
