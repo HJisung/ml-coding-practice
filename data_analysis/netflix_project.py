ffb4787e5acf5a5f9a87a6800dff2cb8ca3ac560
HJisung <wltjd9972@gmail.com>
2026-05-01 18:24:14 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -158,4 +158,6 @@ netflix_age_country[netflix_age_country['title'].str.contains('Sankofa', na=Fals
 
 # 각 나이 그룹에 따른 국가별 넷플릭스 콘텐츠 수 구하기
 netflix_age_country_unstack = netflix_age_country.groupby('age_group')['country'].value_counts().unstack()
-netfli
+netflix_age_country_unstack
+
+# 특정 나이 그룹에 따른 특정 나라별 콘텐츠로 
