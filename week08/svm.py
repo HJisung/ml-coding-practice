7188931a9e26e7bcb2155946a49be875a37d8393
HJisung <wltjd9972@gmail.com>
2026-05-10 17:26:06 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -5,4 +5,6 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklea
+from sklearn.svm import LinearSVC
+
+iris = load_iris(as_frame
