bab915483795c4a898e6c0f9e287c0565d4a6a4e
HJisung <wltjd9972@gmail.com>
2026-05-10 17:40:16 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -57,4 +57,7 @@ X = 2 * np.random.rand(50, 1)
 y = 4 + 3 * X[:, 0] + np.random.randn(50)
 
 svm_reg = make_pipeline(StandardScaler(),
-                        LinearSVR(epsilon=0.5, dual=True, random_st
+                        LinearSVR(epsilon=0.5, dual=True, random_state=42))
+svm_reg.fit(X, y)
+
+from sklearn.svm impor
