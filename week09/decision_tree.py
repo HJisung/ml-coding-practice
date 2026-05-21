85ef56e40a44ef7f4a95967be712692210bf2277
HJisung <wltjd9972@gmail.com>
2026-05-21 22:55:32 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -14,4 +14,6 @@ export_graphviz(
         tree_clf,
         out_file="iris_tree.dot",
         feature_names=["petal length (cm)", "petal width (cm)"],
-        class_names
+        class_names=iris.target_names,
+        rounded=True,
+       
