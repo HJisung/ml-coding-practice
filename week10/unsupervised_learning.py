a942c4fc554726c3e2037ca250563310ed7df45e
HJisung <wltjd9972@gmail.com>
2026-05-31 18:08:55 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -171,4 +171,7 @@ plt.show()
 from sklearn.neighbors import KNeighborsClassifier
 
 X, y = make_moons(n_samples=1000, noise=0.05, random_state=42)
-dbscan = DBSCAN(eps=0.2, min
+dbscan = DBSCAN(eps=0.2, min_samples=5)
+dbscan.fit(X)
+
+knn = KNeighborsClassif
