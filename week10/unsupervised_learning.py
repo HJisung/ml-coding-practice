2cbfaa91ff81e93bee65ab9c4cc074faf5676d1f
HJisung <wltjd9972@gmail.com>
2026-05-31 17:23:58 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -15,4 +15,4 @@ from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 import numpy as np
 
-blob_centers = n
+blob_centers = np.array([[ 0.2,  2.3], [-1.5 ,  2.3], [-2.8,  2.8],
