f1a7ca552adf0e4fac560a6a03a4df9e0c2dd438
HJisung <wltjd9972@gmail.com>
2026-05-31 18:05:23 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -154,3 +154,8 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     plt.title(f"eps={dbscan.eps:.2f}, min_samples={dbscan.min_samples}")
     plt.grid()
     plt.gca().set_axisbelow(True)
+
+dbscan2 = DBSCAN(eps=0.2)
+dbscan2.fit(X)
+
+plt.fi
